39205434048357bad32df3b81094e6bb2191f354
Drew Wyatt <drew.wyatt@onepeloton.com>
2018-11-28 17:02:17 -0500

    ramda cleanup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const glob = require('glob');
+const R = require('ramda');
 
 const IMPORT_REGEX = /import (?:\*.*|{[^}]*}|\w+)?(?: from )?'((?:\w|@|\/|\.)+)';/gm
 const pattern = '../jupiter/packages/@members/class-detail/**.ts?(x)';
@@ -21,37 +22,15 @@ const getFileForPath = path => new Promise((res, rej) => fs.readFile(path, 'utf8
 const getFilesForPaths = paths => Promise.all(paths.map(p => getFileForPath(p)));
 const getMatchesForFile = file => getMatches(file);
 const getMatchesForFiles = files => Promise.resolve(files.map(getMatchesForFile));
-const flatten = arr => Promise.resolve(arr.reduce((a, b) => a.concat(b), []));
+const flatten = arr => Promise.resolve(R.flatten(arr));
 const stripRelative = arr => Promise.resolve(arr.filter(a => a[0] !== '.'));
-const toSet = arr => Promise.resolve(new Set(arr));
+const uniq = arr => Promise.resolve(R.uniq(arr));
 
 getFilePathsForPattern(pattern)
   .then(getFilesForPaths)
   .then(getMatchesForFiles)
   .then(flatten)
   .then(stripRelative)
-  .then(toSet)
+  .then(uniq)
   .then(d => console.log(d))
   .catch(e => console.error(e));
-
-
-// glob('../jupiter/packages/@members/class-detail/**.ts?(x)', null, (err, files) => {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log(files);
-//     files.forEach(path => {
-//       fs.readFile(path, 'utf8', (readErr, data) => {
-//         if (readErr) {
-//           console.error(readErr);
-//         } else {
-//           console.log(`------- file: ${path} -------`);
-//           console.log(data);
-//           console.log('======= matches =======');
-//           console.log(getMatches(data));
-//           console.log('=======================');
-//         }
-//       });
-//     });
-//   }
-// });
