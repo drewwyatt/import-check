ca188b8aaa5e4fc5b1033b50f5f1d063898b97ce
Drew Wyatt <drew.wyatt@onepeloton.com>
2018-12-02 22:57:39 -0500

    moved unit test file

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-expression-statement
 import test from 'ava';
-import { noArgsSpecified } from './utils';
+import { noArgsSpecified } from './misc';
 
 test('noArgsSpecified', t => {
   t.is(noArgsSpecified, false);
